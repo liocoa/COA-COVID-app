11cce69861f9364156dff67f0db7171cc708f14d
Lio <62915202+liocoa@users.noreply.github.com>
2020-08-28 17:14:27 -0400

    initial commit
    Some initial files I've been using for testing. dummyData1 for some numbers to play with is from the drive. plot_drafting.ipynb is a jupyter notebook so I can mess with plots in an environment I'm familiar with. And dashtest.py is the file I've been using for trying to figure out this dash thing. I think I got bootstrap to work?!?!?

---
+++ dashtest.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+
+
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+df = pd.read_csv('dummyData1.csv')
+
+fig1 = px.line(df,x="date",y="active_cases")
+fig2 = px.line(df,x="date",y="current_quar")
+fig3 = px.scatter(df,x="positive",y="quar_in")
+
+
+
+
+
+
+app.layout = dbc.Container(
+    [
+    	dbc.Row(dbc.Col(html.H1("This is a title!"))),
+        dbc.Row(
+        	[
+        		dbc.Col(dcc.Graph(figure=fig1),width=6),
+        		dbc.Col(dcc.Graph(figure=fig2),width=6)
+        	]
+        ),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(figure=fig3))
+            ]
+        ),
+    ],
+
+
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
